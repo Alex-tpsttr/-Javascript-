3d9e8cc82c87b04a622a0cd40e4e781255500bf1
Frichko Oleksandr <frichko18@icloud.com>
2022-07-03 00:54:13 +0300

    Коміт на якийсь ений день вивчання Javascript. Вирішив це все комітити добавляти та більше користуватись git. Щоб повноцінно розвиватись як програміст.

---
+++ js/main.js
@@ -0,0 +1,108 @@
+// const objectA = {
+//     name: 'Aleks',
+//     lastName: 'Frichko',
+//     age: 20,
+// }
+
+// const doubleA = objectA
+
+// doubleA.stuff = 'glovo'
+
+// console.log(objectA)
+
+// console.log('Я працюю!')
+
+// // function yourYearToday() {
+// //     let dateBirthday = prompt('Напишіть ваш рік народження.')
+// //     let todaytime = prompt('Напишіть сьогоднішні рік')
+
+// //     yourBirthday = todaytime - dateBirthday
+
+
+
+// //     console.log(--yourBirthday)
+// // }
+
+// // yourYearToday()
+
+// const myGirls = {
+//     sasha: 23,
+//     angelina: 11,
+// }
+
+// console.log(myGirls)
+
+// myGirls.lena = 5
+
+// myGirls.kate = 14
+
+// myGirls.ola = 90
+
+// myGirls.dasha = 1
+
+// console.log(myGirls)
+
+// delete myGirls.dasha
+
+// console.log(myGirls)
+
+// const myInformation = {
+//     name: 'Aleks',
+//     lastName: 'Frichko',
+
+// }
+
+// console.log(myInformation)
+
+// const myLocation = 'country'
+
+// myInformation[myLocation] = 'Ukraine'
+
+// console.log(myInformation)
+
+
+// let many = 338 
+
+// let iWant = 10000
+
+// const myHave = {
+//     many,
+//     iWant,
+//     shit: 'А не багато хочеш ?'
+// }
+
+// console.log(myHave)
+
+// const myCity = {
+//     city: 'Khm',
+//     popular: false,
+// }
+
+// console.log(myCity)
+
+// myCity.country = 'Ukranine'
+
+// myCity.yearsCity = 600 
+
+// console.log(myCity)
+
+// let nameUser = prompt('Напишіть ваше імя')
+
+// const greetingUser = {
+//     nameUser,
+//     nameGreeating: function () {
+//         console.log(this.nameUser)
+//     }
+// }
+
+// greetingUser.nameGreeating()
+
+// const post = {
+//     title: 'title',
+//     likesVideo: 999
+// }
+
+// let postJson = JSON.stringify(post)
+
+// console.log(post)
+// console.log(postJson)
